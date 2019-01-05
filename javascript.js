7195eaa302baff4a8d0ba9dffd5b672a50803a63
Mikael Fredriksson <224Mikael@gmail.com>
2019-01-05 20:29:18 +0100

    First commit. Unfinished version

---
+++ javascript.js
@@ -0,0 +1,17 @@
+function changeCategory(info, category) {
+  var categoryChildren = document.getElementById('categoryList').children;
+  for (var i = 0; i < categoryChildren.length; i++) {
+    categoryChildren[i].removeAttribute("id");
+  }
+
+  var infoChildren = document.getElementById('infoList').children;
+  for (var i = 0; i < infoChildren.length; i++) {
+    infoChildren[i].style.display = "none";
+  }
+
+
+  var currentInfo = document.getElementById(info);
+  currentInfo.style.display = "block";
+
+  category.setAttribute("id", "active");
+}
